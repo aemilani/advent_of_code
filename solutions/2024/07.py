099b244482bee967f898c854853b8b90e8cc0e02
aemilani <aealieftekhary@gmail.com>
2024-12-16 23:53:11 +0100

    Solved up to problem 8

---
+++ solutions/2024/07.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def do_operation(n1, n2, op):
+    if op == '0':
+        return n1 + n2
+    elif op == '1':
+        return n1 * n2
+    elif op == '2':
+        return int(str(n1) + str(n2))
+
+
+if __name__ == "__main__":
+    with open('../../data/input/07.txt', 'r') as f:
+        string = f.read().strip()
+    lis = string.split('\n')
+
+    sum_true_values = 0
+    for line in lis:
+        value = int(line.split(':')[0])
+        numbers = [int(number) for number in line.split(':')[1].strip().split(' ')]
+        # numbers = line.split(':')[1].strip().split(' ')
+        n_operators = 2  # + and *
+        n_spaces = len(numbers) - 1
+        n_operator_combinations = n_operators ** n_spaces
+
+        for i in range(n_operator_combinations):
+            operators = bin(i)[2:].zfill(n_spaces)
+            result = numbers[0]
+            for operator, n2 in zip(operators, numbers[1:]):
+                result = do_operation(result, n2, operator)
+            if result == value:
+                sum_true_values += value
+                break
+
+    print(f'Part 1: {sum_true_values}')
+
+    sum_true_values = 0
+    for ct, line in enumerate(lis):
+        print(f'{ct} / {len(lis)}')
+        value = int(line.split(':')[0])
+        numbers = [int(number) for number in line.split(':')[1].strip().split(' ')]
+        n_operators = 3  # + and * and ||
+        n_spaces = len(numbers) - 1
+        n_operator_combinations = n_operators ** n_spaces
+
+        for i in range(n_operator_combinations):
+            operators = np.base_repr(i, base=n_operators).zfill(n_spaces)
+            result = numbers[0]
+            for operator, n2 in zip(operators, numbers[1:]):
+                result = do_operation(result, n2, operator)
+            if result == value:
+                sum_true_values += value
+                break
+
+    print(f'Part 2: {sum_true_values}')
---
+++ solutions/2024/08.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+with open('../../data/input/08.txt', 'r') as f:
+    string = f.read().strip()
+lis = string.split('\n')
+mat = np.array([list(elem) for elem in lis])
+
+chars = np.unique(mat)
+antenna_types = np.delete(chars, np.argwhere(chars == '.')[0][0])
+
+anti_coords = []
+for antenna in antenna_types:
+    coords = [(int(coord[0]), int(coord[1])) for coord in list(np.argwhere(mat == antenna))]
+    for i in range(len(coords)):
+        for j in range(i + 1, len(coords)):
+            c1, c2 = coords[i], coords[j]
+            diff = (c2[0] - c1[0], c2[1] - c1[1])
+
+            anti_coord = (c2[0] + diff[0], c2[1] + diff[1])
+            if 0 <= anti_coord[0] < mat.shape[0] and 0 <= anti_coord[1] < mat.shape[1]:
+                anti_coords.append(anti_coord)
+
+            anti_coord = (c1[0] - diff[0], c1[1] - diff[1])
+            if 0 <= anti_coord[0] < mat.shape[0] and 0 <= anti_coord[1] < mat.shape[1]:
+                anti_coords.append(anti_coord)
+
+anti_coords = set(anti_coords)
+print(f'Part 1: {len(anti_coords)}')
+
+anti_coords = []
+for antenna in antenna_types:
+    coords = [(int(coord[0]), int(coord[1])) for coord in list(np.argwhere(mat == antenna))]
+    for i in range(len(coords)):
+        for j in range(i + 1, len(coords)):
+            c1, c2 = coords[i], coords[j]
+            diff = (c2[0] - c1[0], c2[1] - c1[1])
+
+            mul = 0
+            anti_coord = (c2[0] + diff[0] * mul, c2[1] + diff[1] * mul)
+            anti_coords.append(anti_coord)
+            while True:
+                mul += 1
+                anti_coord = (c2[0] + diff[0] * mul, c2[1] + diff[1] * mul)
+                if 0 <= anti_coord[0] < mat.shape[0] and 0 <= anti_coord[1] < mat.shape[1]:
+                    anti_coords.append(anti_coord)
+                else:
+                    break
+
+            mul = 0
+            anti_coord = (c1[0] - diff[0] * mul, c1[1] - diff[1] * mul)
+            anti_coords.append(anti_coord)
+            while True:
+                mul += 1
+                anti_coord = (c1[0] - diff[0] * mul, c1[1] - diff[1] * mul)
+                if 0 <= anti_coord[0] < mat.shape[0] and 0 <= anti_coord[1] < mat.shape[1]:
+                    anti_coords.append(anti_coord)
+                else:
+                    break
+
+anti_coords = set(anti_coords)
+print(f'Part 2: {len(anti_coords)}')
